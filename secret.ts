e006a4730a7a7c3c48567e191f32a9a3b8479ed3
victoria1196 <victoria.sidorchik@ox.security>
2026-04-01 16:03:12 +0300

    Update secret.ts

--- secret.ts
+++ secret.ts
@@ -1,3 +1,3 @@
 export const short_pass = "qwerty123";
 export const GH_PAT_ORIG = "ghp_jrgoU8ts3fbszM5Ln2oH97FGiDX7IU20w4ZN";
-export const GH_PAT = "ghp_jrgoU8ts3fbszM5Ln2oH97FGiDX7IU20w4QY";
+export const GH_PAT = "ghp_jrgoU8ts3fbszM5Ln2oH97FGiDX7IU20w4QR";
