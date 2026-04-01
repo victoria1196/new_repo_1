7d4303100c624adcc83b66da37e80003d1b88280
victoria1196 <victoria.sidorchik@ox.security>
2026-04-01 12:04:08 +0300

    Update GitHub PAT to a new value

--- secret.ts
+++ secret.ts
@@ -1,3 +1,3 @@
 xport const short_pass = "qwerty123";
 export const GH_PAT_ORIG = "ghp_jrgoU8ts3fbszM5Ln2oH97FGiDX7IU20w4ZN";
-export const GH_PAT = "ghp_jrgoU8ts3fbszM5Ln2oH97FGiDX7IU20w4QW";
+export const GH_PAT = "ghp_jrgoU8ts3fbszM5Ln2oH97FGiDX7IU20w4QE";
